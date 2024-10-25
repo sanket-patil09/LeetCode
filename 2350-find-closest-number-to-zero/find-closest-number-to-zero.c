035e0adfa16474b5d7e2326e6da39e4ee4e0d122
sanket-patil09 <patilsanketshri12@gmail.com>
2024-10-25 19:57:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (98.31%) - LeetSync

--- 2350-find-closest-number-to-zero/find-closest-number-to-zero.c
+++ 2350-find-closest-number-to-zero/find-closest-number-to-zero.c
@@ -1,16 +1,18 @@
 int findClosestNumber(int* nums, int numsSize) {
-    if (numsSize == 0) {
-        return 0;  // Return 0 if array is empty
+    if (numsSize == 0) 
+    {
+        return 0;  
     }
 
-    int closest = nums[0];  // Assume the first element is the closest initially
+    int closest = nums[0];  
 
-    for (int i = 1; i < numsSize; i++) {
-        // If current number is closer to zero, update closest
+    for (int i = 1; i < numsSize; i++) 
+    {
+        
         if (abs(nums[i]) < abs(closest)) {
             closest = nums[i];
         } 
-        // If the absolute values are the same, choose the positive number
+        
         else if (abs(nums[i]) == abs(closest) && nums[i] > closest) {
             closest = nums[i];
         }
